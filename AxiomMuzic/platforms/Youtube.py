86455b07349f1b9b5f37c5b304b7fc0b7b5ac871
The Maanav <prettylioness0550@gmail.com>
2026-06-12 14:38:55 +0530

    AxiomBots

--- AxiomMuzic/platforms/Youtube.py
+++ AxiomMuzic/platforms/Youtube.py
@@ -1,5 +1,3 @@
-
-
 import asyncio
 import os
 import re
@@ -10,9 +8,9 @@ from pyrogram.types import Message
 from py_yt import VideosSearch, Playlist
 import aiohttp
 
-API_URL = os.environ.get("API_URL", "https://api.shrutibots.site")
+API_URL = os.environ.get("SHRUTI_API_URL", "https://api.shrutibots.site")
 
-API_KEY = os.environ.get("API_KEY", "") ## Get This API KEY FROM: @SHRUTIAPIBOT 
+API_KEY = os.environ.get("SHRUTI_API_KEY", "ShrutiBotsx7llDDmWnF986ygx9seT") ## Get This API KEY FROM TELEGRAM BOT USERNAME: @SHRUTIAPIBOT 
 
 DOWNLOAD_DIR = "downloads"
 
