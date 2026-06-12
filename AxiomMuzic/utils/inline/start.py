070bb29b196cab4b1cf45a19b8916214484d3418
The Maanav <prettylioness0550@gmail.com>
2026-06-12 16:27:41 +0530

    AxiomBots

--- AxiomMuzic/utils/inline/start.py
+++ AxiomMuzic/utils/inline/start.py
@@ -1,17 +1,27 @@
 
 
+import random
 from pyrogram.types import InlineKeyboardButton
+from pyrogram.enums import ButtonStyle
+
 import config
-from AxiomMuzic import app
+from AxiomMusic import app
+
+def axiombtn():
+    return random.choice([
+        ButtonStyle.SUCCESS,
+        ButtonStyle.DANGER,
+        ButtonStyle.PRIMARY
+    ])
 
 
 def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true", style=axiombtn(),
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT, style=axiombtn(),),
         ],
     ]
     return buttons
@@ -22,17 +32,17 @@ def private_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                url=f"https://t.me/{app.username}?startgroup=true", style=axiombtn(),
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID, style=axiombtn(),),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL, style=axiombtn(),),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["S_B_10"], callback_data="OwnerAxiom"),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT, style=axiombtn(),),
+            InlineKeyboardButton(text=_["S_B_10"], callback_data="OwnerAxiom", style=axiombtn(),),
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="AxiomOwner")],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="AxiomOwner", style=axiombtn(),)],
     ]
     return buttons
