84a809d123ab7f00719f2b57de869ef730fb7f55
The Maanav <prettylioness0550@gmail.com>
2026-06-14 16:56:31 +0530

    AxiomBots

--- AxiomMuzic/utils/inline/start.py
+++ AxiomMuzic/utils/inline/start.py
@@ -35,14 +35,14 @@ def private_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true", style=axiombtn(),
             )
         ],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="AxiomOwner", style=axiombtn(),)],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID, style=axiombtn(),),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID, style=ButtonStyle.SUCCESS),
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL, style=axiombtn(),),
         ],
         [
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT, style=axiombtn(),),
             InlineKeyboardButton(text=_["S_B_10"], callback_data="OwnerAxiom", style=axiombtn(),),
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="AxiomOwner", style=axiombtn(),)],
     ]
     return buttons
