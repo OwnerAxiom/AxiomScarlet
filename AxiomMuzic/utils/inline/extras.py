3372a7c4928029c64cf95d9f4dcbcb4ac546e097
The Maanav <prettylioness0550@gmail.com>
2026-06-18 11:00:39 +0530

    Update extras.py

--- AxiomMuzic/utils/inline/extras.py
+++ AxiomMuzic/utils/inline/extras.py
@@ -11,15 +11,24 @@
 #
 # ❤️ Made with dedication and love by AxiomBots
 # -----------------------------------------------
+
+import random
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.enums import ButtonStyle
 from config import SUPPORT_CHAT
 
-
+def random_style():
+    return random.choice([
+        ButtonStyle.SUCCESS,
+        ButtonStyle.DANGER,
+        ButtonStyle.PRIMARY
+    ])
+    
 def botplaylist_markup(_):
     buttons = [
         [
-            InlineKeyboardButton(text=_["S_B_9"], url=SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            InlineKeyboardButton(text=_["S_B_6"], url=SUPPORT_CHAT, style=random_style()),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close", style=ButtonStyle.DANGER),
         ],
     ]
     return buttons
@@ -32,6 +41,7 @@ def close_markup(_):
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
                     callback_data="close",
+                    style=ButtonStyle.DANGER,
                 ),
             ]
         ]
@@ -44,8 +54,9 @@ def supp_markup(_):
         [
             [
                 InlineKeyboardButton(
-                    text=_["S_B_9"],
+                    text=_["S_B_6"],
                     url=SUPPORT_CHAT,
+                    style=random_style(),
                 ),
             ]
         ]
